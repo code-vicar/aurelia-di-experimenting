6d30299d2b3714da67ddd855915b573979c0eae6
Scott Vickers <svickers@weareratio.com>
2016-04-26 10:38:48 -0700

    create configure module

---
+++ configure.js
@@ -0,0 +1,5 @@
+require('core-js/es6/reflect');
+require('reflect-metadata');
+var di = require('aurelia-dependency-injection');
+var container = new di.Container();
+container.makeGlobal();
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-require('core-js/es6/reflect');
-require('reflect-metadata');
+require('./configure')
 var di = require('aurelia-dependency-injection');
 var Factory = di.Factory;
 
-var container = new di.Container();
-container.makeGlobal();
-
 var thingOne = require('./thingOne');
 
-var thingOneFactory = container.get(Factory.of(thingOne));
+var thingOneFactory = di.Container.instance.get(Factory.of(thingOne));
 
 var thing = new thingOneFactory('hello');
 
